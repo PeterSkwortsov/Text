8795ec8d2223643524261f6dd33bb1adf8bc9d8d
Peter <skwortsow.petia@yandex.ru>
2025-02-10 18:12:41 +0300

    Падающий куб,сфера со звуком

--- main.js
+++ main.js
@@ -2,13 +2,64 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'; 
 import GUI from 'lil-gui';
 import { materialColor } from 'three/tsl';
-import gsap from 'gsap';
-import CANNON from 'cannon';
+import * as CANNON from 'cannon-es';
+
 
 const gui = new GUI()
+const debugObject = {}
+debugObject.createSphery = () => {
 
+    createSphery(
+        Math.random() * 0.5,
+        {
+            x: Math.random() - 0.5,
+            y: 2,
+            z: Math.random() - 0.5 * 2
+        }
+    )
+}
+debugObject.createBox = () => {
+    createBox(
+        Math.random() * 0.5,
+        Math.random() * 0.5,
+        Math.random() * 0.5,    
+     {
+        x: Math.random() - 0.5 * 3,
+        y: 2.5,
+        z: Math.random() - 0.5 * 3
+        
+    }
+)
+}
+
+debugObject.reset = () => {
+    for (const object of objectToUpdate) {
+        world.removeBody(object.body)
+        scene.remove(object.mesh)
+    }
+}
+
+gui.add(debugObject, 'createSphery')
+gui.add(debugObject, 'createBox')
+gui.add(debugObject, 'reset')
 
 const scene = new THREE.Scene();
+
+const hitSound = new Audio('static/textures/metallicheskih.mp3');
+const playSound = (collision) => {
+
+    const impactStrength = collision.contact.getImpactVelocityAlongNormal();
+    
+    console.log(impactStrength)
+
+    if (impactStrength > 1.5) {
+        hitSound.volume = Math.random()
+        hitSound.currentTime = 0
+        hitSound.play();
+    }
+
+    
+}
 
 const textureLoader = new THREE.TextureLoader();
 
@@ -44,29 +95,25 @@ planeMesh.rotation.x = Math.PI / 180 * -90
 planeMesh.receiveShadow = true
 scene.add(planeMesh);
 
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.3, 32, 32),
-    new THREE.MeshToonMaterial({
-        color: 0xffffff,
-        emissiveIntensity: 0.2, 
-        gradientMap: mapTexture,
-    })
-)
-sphere.position.set(0, 1, 0)
-sphere.castShadow = true
-scene.add(sphere);
+
 
 const world = new CANNON.World();
+world.broadphase = new CANNON.SAPBroadphase(world); // оптимизация при использовании множества объектов по одной оси
+world.allowSleep = true  // важно! добавляй этот параметр чтобы кэнон не просчитывал объекты без движения
 world.gravity.set(0, -8.82, 0);
 
-const sphereShape = new CANNON.Sphere(0.3);
-const sphereBody = new CANNON.Body({
-    mass: 1,
-    position: new CANNON.Vec3(0, 5, 0),
-    shape: sphereShape
+
+const defaultMaterial = new CANNON.Material('default');
+
+const defaultContactMaterial = new CANNON.ContactMaterial(defaultMaterial, defaultMaterial, {
+    friction: 0.1,
+    restitution: 0.5
 })
 
-world.addBody(sphereBody);
+world.addContactMaterial(defaultContactMaterial);
+world.defaultContactMaterial = defaultContactMaterial
+
+
 
 const floorShape = new CANNON.Plane();
 const floorBody = new CANNON.Body()
@@ -85,6 +132,79 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     
 });
+
+const objectToUpdate = []
+
+const sphereGeometry = new THREE.SphereGeometry(1, 20, 20)
+const sphereMaterial = new THREE.MeshToonMaterial({
+    gradientMap: mapTexture
+})
+
+// обрати внимание на то что у нас радиус - 1, но благодаря тому что mesh.scale.set(radius, radius, radius) - поолучается рандом
+
+const createSphery = (radius, position) => {
+    const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial)
+
+    mesh.scale.set(radius, radius, radius)
+    mesh.castShadow = true
+    mesh.position.copy(position)
+    scene.add(mesh)
+
+    const shape = new CANNON.Sphere(radius)
+    const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0,3,0),
+        shape: shape,
+        material: defaultMaterial,
+    })
+    body.position.copy(position)
+    body.addEventListener('collide', playSound)
+
+    world.addBody(body)
+
+    objectToUpdate.push({
+        mesh: mesh,
+        body: body
+    })
+}
+
+createSphery(0.30, {x: 0, y: 0.8, z: 0})
+
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const boxMaterial = new THREE.MeshStandardMaterial({
+    metalness: 0.3,
+    roughness: 0.5
+})
+ 
+const createBox = (width, height, depth, position) => {
+    const mesh = new THREE.Mesh(boxGeometry, boxMaterial)
+
+    mesh.scale.set(width, height, depth)
+    mesh.castShadow = true
+    mesh.position.copy(position)
+    scene.add(mesh)
+
+    const shape = new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5))
+    const body = new CANNON.Body({
+        mass: 2,
+        position: new CANNON.Vec3(0, 3, 0),
+        shape: shape,
+        material: defaultMaterial,
+    })
+    body.addEventListener('collide', playSound)
+    body.position.copy(position)
+    world.addBody(body)
+
+    objectToUpdate.push({
+        mesh: mesh,
+        body: body
+    })
+}
+
+
+
+
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -103,6 +223,14 @@ let oldElapsedTime = 0;
 
 const clock = new THREE.Clock();
 
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(sizes.width, sizes.height);
+    
+})
 
 const tick = () => {    
     const elapsedTime = clock.getElapsedTime();
@@ -116,14 +244,18 @@ const tick = () => {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     
     const deltaTime = elapsedTime - oldElapsedTime;
-oldElapsedTime = elapsedTime;
+    oldElapsedTime = elapsedTime;
 
-world.step(1 / 60, deltaTime, 3);
-sphere.position.copy(sphereBody.position);
+    
+    world.step(1 / 60, deltaTime, 3);
+
+    for (const object of objectToUpdate) {
+        object.mesh.position.copy(object.body.position)
+        object.mesh.quaternion.copy(object.body.quaternion)
+    }
 
 
-
-controls.update();
+    controls.update();
 }
 
 tick();
