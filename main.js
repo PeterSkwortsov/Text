374f367ba9ad911c01774dce2084b6e429f30ff9
Peter <skwortsow.petia@yandex.ru>
2025-02-18 16:38:45 +0300

    Карта окружения HDR

--- main.js
+++ main.js
@@ -1,38 +1,46 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
 
 const scene = new THREE.Scene()
+
 
 const gui = new GUI()
 const global = {}
 
 const cubeTextureLoader = new THREE.CubeTextureLoader()
-
-const environmentMap = cubeTextureLoader.load([
-    'static/textures/box/px.png',
-    'static/textures/box/nx.png',
-    'static/textures/box/py.png',
-    'static/textures/box/ny.png',
-    'static/textures/box/pz.png',
-    'static/textures/box/nz.png',
-])
-
-scene.environment = environmentMap
-scene.background = environmentMap
+const rgbeLoader = new RGBELoader()
 
 
-const directionallight = new THREE.DirectionalLight(0xebfeff, Math.PI)
-directionallight.position.set(1, 0.1, 1)
-directionallight.visible = false
-scene.add(directionallight)
+// const environmentMap = cubeTextureLoader.load([
+//     'static/textures/box/px.png',
+//     'static/textures/box/nx.png',
+//     'static/textures/box/py.png',
+//     'static/textures/box/ny.png',
+//     'static/textures/box/pz.png',
+//     'static/textures/box/nz.png',
+// ])
 
-const ambientLight = new THREE.AmbientLight(0xebfeff, Math.PI / 16)
-ambientLight.visible = false
+// scene.environment = environmentMap
+// scene.background = environmentMap
+
+rgbeLoader.load('static/textures/box/brown_photostudio_01_2k.hdr', (environmentMap) => {
+    environmentMap.mapping = THREE.EquirectangularReflectionMapping
+    scene.background = environmentMap
+})
+
+// const directionallight = new THREE.DirectionalLight(0xebfeff, Math.PI)
+// directionallight.position.set(1, 0.1, 1)
+// directionallight.visible = true
+// scene.add(directionallight)
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 3)
+ambientLight.visible = true
 scene.add(ambientLight)
 
-const camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
 camera.position.set(-1.1, 2.4, 1.3)
 
 const renderer = new THREE.WebGLRenderer({ antialias: true })
@@ -70,7 +78,7 @@ const updateAllMaterial = () =>
         if (child.isMesh && child.material.isMeshStandardMaterial) 
             {
             child.material.environmentIntensity = global.environmentIntensity
-            child.scale.set(2, 2, 2)
+            child.scale.set(2.5, 2.5, 2.5)
         }
     })
 }
@@ -83,11 +91,7 @@ new GLTFLoader().load('static/fly/flyModel/FlightHelmet.gltf', (gltf) => {
 })
 
 
-
-
-
-
-gui.add(scene, 'environmentIntensity').min(0).max(5).step(0.01)
+gui.add(scene, 'environmentIntensity').min(0).max(10).step(0.01)
 
 gui.add(renderer, 'toneMappingExposure').min(0).max(5).step(0.01)
 
